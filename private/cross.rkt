8298e0f85e6b47abe09274384fd1630b9fb25562
Antigen-1 <official@antigen.top>
2022-04-14 10:28:02 +0800

    fix a bug

--- private/cross.rkt
+++ private/cross.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
+(require (prefix-in o: racket/base))
 (provide (all-defined-out))
 
 ;;newline
-(define read-line (lambda ([in (current-input-port)]) (read-line in 'any)))
-(define read-bytes-line (lambda ([in (current-input-port)]) (read-bytes-line in 'any)))
+(define read-line (lambda ([in (current-input-port)]) (o:read-line in 'any)))
+(define read-bytes-line (lambda ([in (current-input-port)]) (o:read-bytes-line in 'any)))
