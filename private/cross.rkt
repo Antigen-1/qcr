0ddf841873f7255982c6972e7ea908d96adf8fbf
Antigen-1 <official@antigen.top>
2022-04-16 17:34:36 +0800

    construct sync

--- private/cross.rkt
+++ private/cross.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
-(require (prefix-in o: racket/base))
+(require (prefix-in o: racket/base)
+         (prefix-in o: racket/port))
 (provide (all-defined-out))
 
 ;;newline
 (define read-line (lambda ([in (current-input-port)]) (o:read-line in 'any)))
 (define read-bytes-line (lambda ([in (current-input-port)]) (o:read-bytes-line in 'any)))
+(define read-line-evt (lambda ([in (current-input-port)]) (o:read-line-evt in 'any)))
